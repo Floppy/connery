7dfcb2a8f6879f71d378ea4fe620882dff17e76a
James Smith <james@floppy.org.uk>
2012-09-21 01:22:08 +0300

    added initial conversion script

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
-connery
+Connery
 =======
 
-a handy tool for converting data from AMEE dataset to calcJSON
+A handy tool for converting data from AMEE datasets to calcJSON format
+
+* [AMEE datasets](https://github.com/AMEE/datasets)
+* [calcJSON](https://github.com/spatchcock/calcJSON)
+* [calc-json-ruby](https://github.com/spatchcock/calc-json-ruby)
+
+Doesn't handle 100% of the schema conversion right now, but does all the basics - inputs, contexts, and algorithms.
+
+Run like:
+
+ruby connery.rb /path/to/amee/dataset /path/to/output/with/basename
+
+This will save the dataset in the first directory to basename.json and basename.csv in the 'with' directory.
---
+++ connery.rb
@@ -0,0 +1,83 @@
+require 'csv'
+require 'json'
+
+input_path = ARGV[0]
+output_name = ARGV[1]
+
+schema = {}
+
+algo_file = nil
+headers = []
+itemdefs = []
+
+# read itemdef.csv
+CSV.foreach(File.join(input_path,'itemdef.csv')) do |row|
+  if row[0] == 'name' && schema['name'].nil?
+    schema['name'] = row[1]
+  else  
+    case row[0]
+    when 'algFile'
+      algo_file = row[1]
+    else
+      if headers.empty?
+        headers = row
+      else
+        hash = {}
+        row.each_with_index do |cell, i|
+          hash[headers[i]] = cell
+        end
+        itemdefs << hash
+      end
+    end
+  end
+end
+
+def role(itemdef)
+  if itemdef['isDrillDown'] == 'true'
+    'context'
+  elsif itemdef ['isDataItemValue'] == 'true'
+    'parameter'
+  else
+    'variable'
+  end
+end
+
+schema['definitions'] = itemdefs.map do |itemdef|
+  {
+    'label' => itemdef['path'],
+    'name' => itemdef['name'],
+    'type' => itemdef['type'].downcase,
+    'role' => role(itemdef),
+    'default' => itemdef['default'],
+    'unit' => itemdef['unit'], # needs perUnit integration
+  }
+end
+
+# Add outputs
+implicit_output = true
+schema['definitions'] << {
+  'label' => 'co2e',
+  'name' => 'CO2 equivalent',
+  'role' => 'output',
+  'unit' => 'kg'
+}
+
+# Copy data.csv
+File.open(output_name+'.csv', "wb") do |file|
+  file << File.read(File.join(input_path,'data.csv'))
+end
+
+# Algorithm
+schema['algorithm'] = File.read(File.join(input_path,algo_file))
+if implicit_output
+  schema['algorithm'] = <<EOF
+  co2e = eval(#{schema['algorithm'].to_json})
+EOF
+end
+
+File.open(output_name+'.json', "wb") do |file|
+  file << JSON.pretty_generate(schema)
+end
+
+
+
